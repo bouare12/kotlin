a8880c894c9dfddbb8955e23eee4e1529fed36de
Bandiougou BOUARE <bandiougoubouare@yahoo.fr>
2023-02-02 00:32:19 +0100

    Correction boucle for

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -9,12 +9,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val names = Array<String>(4) {""}
-        names.set(0, "Tom")
-        println(names[0])
+        for (i in 1..6) {
+           println("Affichage de i ${i}")
+        }
 
-        val ages = arrayOf(4,6,3)
-        println(ages.get(2))
+        for (i in 1..6 step 2) {
+            println("affiche de i par step de 2 ${i}")
+        }
+
+        for (i in 6 downTo 1) {
+            println("Affiche i de façon decroissante ${i}")
+        }
+
+        val animaux: Array<String> = arrayOf("Chat", "Chien", "vache", "cheval")
+
+        for(i in 0..animaux.size) {
+            println("Affichage des animaux avec for ${animaux[i]}")
+        }
+
+        for (animal in animaux) {
+            println("Affichage des animaux avec foreach ${animal}")
+        }
 
     }
 }
