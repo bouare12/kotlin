7df22b58362215293e9470ef04ce7a2dc30de74d
bouare12 <bandiougoubouare@yahoo.fr>
2023-02-05 23:54:19 +0100

    Correction classes et constructor

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -6,13 +6,30 @@ import android.os.Bundle
 import com.example.helloword.common.name
 import com.example.helloword.common.personne
 
+class Pers( name : String,  age: Int, poids: Float) {
+
+    val canPlayFoot = age > 6 && poids > 15.60f
+    init {
+        val footPlayer = when(canPlayFoot) {
+            true -> "Joue au football"
+            false -> "Ne peut pas jouer au foot"
+        }
+        println("Info personne : $name , $age, $footPlayer")
+    }
+
+    constructor(name: String): this(name, 5, 5.1f)
+    fun danse() {
+        println("La personne danse bien au Tango")
+    }
+
+}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // name = "Jerry"
-        println("Name importer est $name")
-        personne()
+        val personne = Pers("tom", 10, 20.70f)
+        var personne1 = Pers("Jerry")
+        println(personne1.danse())
     }
 }
