8ab7d1a69a240b55403d565a4be19b74db09ce17
bouare12 <bandiougoubouare@yahoo.fr>
2023-02-06 00:39:16 +0100

    Correction héritage

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -6,30 +6,23 @@ import android.os.Bundle
 import com.example.helloword.common.name
 import com.example.helloword.common.personne
 
-class Pers( name : String,  age: Int, poids: Float) {
+open class Vehicule(val nbrDeRoue: Int) {
 
-    val canPlayFoot = age > 6 && poids > 15.60f
-    init {
-        val footPlayer = when(canPlayFoot) {
-            true -> "Joue au football"
-            false -> "Ne peut pas jouer au foot"
-        }
-        println("Info personne : $name , $age, $footPlayer")
+    fun showNbrDeRoue () {
+        println("Le nombre de roues est : $nbrDeRoue")
     }
-
-    constructor(name: String): this(name, 5, 5.1f)
-    fun danse() {
-        println("La personne danse bien au Tango")
-    }
-
 }
+class Voiture: Vehicule(4)
+class Tricycle: Vehicule(3)
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val personne = Pers("tom", 10, 20.70f)
-        var personne1 = Pers("Jerry")
-        println(personne1.danse())
+        val voiture = Voiture()
+        voiture.showNbrDeRoue()
+        val triclycle = Tricycle()
+        triclycle.showNbrDeRoue()
     }
 }
