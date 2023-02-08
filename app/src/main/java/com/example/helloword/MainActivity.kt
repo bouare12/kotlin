e15e6043274da2b618cfef7b6d6ad80ecdaf0af8
bouare12 <bandiougoubouare@yahoo.fr>
2023-02-08 23:46:57 +0100

    Correction data classe

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -6,23 +6,30 @@ import android.os.Bundle
 import com.example.helloword.common.name
 import com.example.helloword.common.personne
 
-open class Vehicule(val nbrDeRoue: Int) {
+data class Personne (val  name: String, val age: Int) {
 
-    fun showNbrDeRoue () {
-        println("Le nombre de roues est : $nbrDeRoue")
-    }
 }
-class Voiture: Vehicule(4)
-class Tricycle: Vehicule(3)
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val voiture = Voiture()
-        voiture.showNbrDeRoue()
-        val triclycle = Tricycle()
-        triclycle.showNbrDeRoue()
+        // fonction tostring Ex
+        val tom = Personne("Tom", 7)
+        println("toString info sur $tom")
+
+        val jerry = Personne("Jerry", 4)
+        println("mtoString info sur $jerry")
+
+        // fonction copy
+        val mario = tom.copy("Mario")
+        println("mtoString info sur $mario")
+
+        //fonction equal
+        if (tom == mario) {
+            println("ils sont pareills")
+        } else {
+            println("ils sont differents")
+        }
     }
 }
