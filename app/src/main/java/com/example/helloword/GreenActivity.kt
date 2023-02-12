cddea607ec72972d64ff34de4972a7795cea9d80
bouare12 <bandiougoubouare@yahoo.fr>
2023-02-13 00:23:16 +0100

    Correction Intent structures

--- app/src/main/java/com/example/helloword/GreenActivity.kt
+++ app/src/main/java/com/example/helloword/GreenActivity.kt
@@ -2,10 +2,14 @@ package com.example.helloword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class GreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_green)
+
+        val action = intent.action
+        Log.i("GreenActivity", "Action: $action")
     }
 }
--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.helloword
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 
 import com.example.helloword.common.name
@@ -11,7 +10,6 @@ import com.example.helloword.common.personne
 
 class MainActivity : AppCompatActivity() {
 
-    val TAGS = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,13 +17,9 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<TextView>(R.id.start_activity_green)
         button.setOnClickListener {
             val intent = Intent(this, GreenActivity::class.java)
+            // Ajout des actions à intent
+            intent.action = Intent.ACTION_VIEW
             startActivity(intent)
         }
-        Log.v(TAGS, "Verbose message")
-        Log.d(TAGS, "Debug message")
-        Log.i(TAGS, "Info message")
-        Log.w(TAGS, "Warning message")
-        Log.e(TAGS, "Error message")
-        Log.println(Log.ASSERT, TAGS,"Assert message")
     }
 }
