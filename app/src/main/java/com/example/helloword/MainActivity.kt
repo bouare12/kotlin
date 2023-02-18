02e18fc8a3b1111f3246ecb2c7b5965e117b88fd
bouare12 <bandiougoubouare@yahoo.fr>
2023-02-18 01:23:41 +0100

    Correction composants UI de Base

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.helloword
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.my_text).setOnClickListener {
+            Log.i("TestTest", "je viens de cliquer")
+        }
     }
 }
