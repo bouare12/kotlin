77173533bcbb2a08a62cd7df9416f27b178134c1
bouare12 <bandiougoubouare@yahoo.fr>
2023-02-21 01:24:43 +0100

    Correction recuperation des données OptionMenu

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +18,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_add -> {
+                Toast.makeText(this, "Ajouter", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_delete -> {
+                Toast.makeText(this, "Supprimer", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_save -> {
+                Toast.makeText(this, "Enregister", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_help -> {
+                Toast.makeText(this, "Aide", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
