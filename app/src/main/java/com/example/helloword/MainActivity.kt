88c9bd13870cef2683c90af7b4f4b3bcafa1d715
bouare12 <bandiougoubouare@yahoo.fr>
2023-02-23 02:30:32 +0100

    Correction TP RecyclerView pour les notes

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.helloword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var notes: MutableList<Note>
+    lateinit var adapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notes = mutableListOf<Note>()
+        notes.add(Note("Note 1", "tototototatatat"))
+        notes.add(Note("Memo Revision", "aaaeazeazrzarazr"))
+        notes.add(Note("Mémo Sport", "zefzefezf"))
+        notes.add(Note("Mémo Jeu", "fzefzefze"))
+        notes.add(Note("Mémo Voyage", "ldlmeflef"))
+
+        adapter = NoteAdapter(notes, this)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.notes_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onClick(view: View) {
+        if (view.tag != null) {
+            Log.i("NotesListActivity","Click sur une")
+        }
     }
 }
