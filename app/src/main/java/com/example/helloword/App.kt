3f99292d08262f4e0bb25ea478f4330d66879a00
bouare12 <bandiougoubouare@yahoo.fr>
2023-03-26 19:17:31 +0200

    Application Météo Création de la base de donnée

---
+++ app/src/main/java/com/example/helloword/App.kt
@@ -0,0 +1,18 @@
+package com.example.helloword
+
+import android.app.Application
+
+class App: Application() {
+
+    companion object {
+        lateinit var instance: App
+        val database: Database by lazy {
+            Database(instance)
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.helloword
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
