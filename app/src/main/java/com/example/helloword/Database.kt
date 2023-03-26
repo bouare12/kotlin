0327e7dbafab910c4baa6577fcd1db42d1b21717
bouare12 <bandiougoubouare@yahoo.fr>
2023-03-26 19:30:08 +0200

    Application Météo Création d'une ville dans la bd

--- app/src/main/java/com/example/helloword/Database.kt
+++ app/src/main/java/com/example/helloword/Database.kt
@@ -1,8 +1,11 @@
 package com.example.helloword
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import com.example.helloword.city.City
 
 private const val DATABASE_NAME = "weather.bd"
 private const val DATABASE_VERSION = 1
@@ -23,6 +26,18 @@ class Database(context: Context):  SQLiteOpenHelper(context, DATABASE_NAME, null
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
+    }
+
+    fun createCity(city: City) : Boolean {
+        val values = ContentValues()
+        values.put(CITY_KEY_NAME, city.name)
+
+        Log.d("Création Data","Creation city: $values")
+
+        val id = writableDatabase.insert(CITY_TABLE_NAME, null, values)
+        city.id = id
+
+        return id > 0
     }
 
 
