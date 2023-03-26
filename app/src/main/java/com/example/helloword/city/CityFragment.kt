fa0e4aa0ad3d982e4126182bebd6cce6c2d52786
bouare12 <bandiougoubouare@yahoo.fr>
2023-03-26 19:53:48 +0200

    Application Météo Création de CityFragment pour la création d'une ville

---
+++ app/src/main/java/com/example/helloword/city/CityFragment.kt
@@ -0,0 +1,19 @@
+package com.example.helloword.city
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.helloword.R
+
+class CityFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_city, container, false)
+        return view
+    }
+}
