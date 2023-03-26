242d51a18a8173af29cbb5f50b4a83ec6ad1512f
bouare12 <bandiougoubouare@yahoo.fr>
2023-03-26 16:19:41 +0200

    Réinitialisation du project pour Application Météo

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -1,118 +1,14 @@
 package com.example.helloword
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.os.Parcelable
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.helloword.utils.deleteNotePserist
-import com.example.helloword.utils.loadNotes
-import com.example.helloword.utils.persistNote
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 
 @Suppress("DEPRECATION")
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(){
 
-    lateinit var notes: MutableList<Note>
-    lateinit var adapter: NoteAdapter
-    lateinit var coordinateurLayout : CoordinatorLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        findViewById<FloatingActionButton>(R.id.create_note_fab).setOnClickListener(this)
-
-        notes = loadNotes(this)
-
-        adapter = NoteAdapter(notes, this)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.notes_recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-
-        coordinateurLayout = findViewById(R.id.coordinator_layout)
     }
 
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != Activity.RESULT_OK || data == null) {
-            return
-        }
-        when (requestCode) {
-            NoteDetailActivity.REQUES_EDIT_NOTE -> processEditNoteResult(data)
-        }
-    }
-
-    override fun onClick(view: View) {
-        if (view.tag != null) {
-            showNoteDetail(view.tag as Int)
-        } else {
-            when(view.id) {
-                R.id.create_note_fab -> createNewNote()
-            }
-        }
-    }
-
-    private fun createNewNote() {
-        showNoteDetail(-1)
-    }
-
-    private fun processEditNoteResult(data: Intent) {
-        val noteIndex = data.getIntExtra(NoteDetailActivity.EXTRAT_NOTE_INDEX, 0)
-        when (data.action) {
-            NoteDetailActivity.ACTION_SAVE_NOTE -> {
-                val note = data.getParcelableExtra<Note>(NoteDetailActivity.EXTRAT_NOTE)
-                if (note != null) {
-                    saveNote(note, noteIndex)
-                }
-            }
-            NoteDetailActivity.ACTION_DELETE_NOTE -> {
-                println("Index For Delete est $noteIndex")
-                deleteNote(noteIndex)
-            }
-        }
-        
-    }
-
-
-    private fun saveNote(note: Note, noteIndex: Int) {
-        persistNote(this, note)
-        if (noteIndex < 0) {
-            notes.add(0,note)
-        } else {
-            notes[noteIndex] = note
-        }
-        adapter.notifyDataSetChanged()
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    private fun deleteNote(noteIndex: Int) {
-        if (noteIndex < 0 ) {
-            return
-        }else{
-            val note = notes.removeAt(noteIndex)
-            deleteNotePserist(this, note)
-            adapter.notifyDataSetChanged()
-            Snackbar.make(coordinateurLayout, "${note.title} supprimé", Snackbar.LENGTH_SHORT).show()
-        }
-    }
-
-    fun showNoteDetail(noteIndex: Int) {
-        val note = if (noteIndex < 0) Note() else notes[noteIndex]
-        val intent = Intent(this, NoteDetailActivity::class.java)
-        intent.putExtra(NoteDetailActivity.EXTRAT_NOTE, note as Parcelable)
-        intent.putExtra(NoteDetailActivity.EXTRAT_NOTE_INDEX, note as Parcelable)
-        startActivityForResult(intent, NoteDetailActivity.REQUES_EDIT_NOTE)
-    }
 }
